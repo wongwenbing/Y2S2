02422ad0a7af6b0edf31e7270883ca596815d2ca
wongwenbing <wongwenbing0718@gmail.com>
2024-11-19 00:11:50 +0800

    ss

---
+++ React Dev/Dev/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Form from 'react-bootstrap/Form'
+import InputGroup from 'react-bootstrap/InputGroup'
+import Button from 'react-bootstrap/Button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Imagene</h1>
+      <Form.Control size="lg" type="text" placeholder="Key in Game Code" />
+      
+      <Button variant="primary" id="join-game">Join Game</Button>
+    </>
+  )
+}
+
+export default App
