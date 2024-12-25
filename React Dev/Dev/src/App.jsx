ea2a01827666e839d6a57338b75400ccd1ff0198
wongwenbing <wongwenbing0718@gmail.com>
2024-12-26 00:27:07 +0800

    COMMITS

--- React Dev/Dev/src/App.jsx
+++ React Dev/Dev/src/App.jsx
@@ -6,23 +6,40 @@ import Form from 'react-bootstrap/Form'
 import InputGroup from 'react-bootstrap/InputGroup'
 import Button from 'react-bootstrap/Button'
 
-function App() {
+function Starting() {
   const [count, setCount] = useState(0)
   document.body.style='background: #006ED3;';
 
   return (
     <div className="sg">
       <h1>Imagene</h1>
-      <Form.Control size="lg" id="game" type="text" placeholder="Key in Game Code" />
+      <Form.Control size="lg" id="game" type="text" placeholder="Key in Game Code!" />
       <br />
-      <Button variant="primary" id="join-game" onClick={}>Join Game</Button>
+      <Button variant="primary" id="join-game">Join Game!</Button>
     </div>
   )
 }
 
-function joingame() {
-  once done go to next page
+function AddName() {
+  return (
+    <div classname="name">
+      <h1>Imagene</h1>
+      <Form.Control size="lg" id="game" type="text" placeholder="Key in your name!" />
+      <br />
+      <Button variant="primary" id="join-game">Continue!</Button>
+    </div>
+  )
 }
 
+function App() {
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<Starting />} /> 
+        <Route path="/additional" element={<AddName />} />
+      </Routes>
+    </Router>
+  )
+}
 
 export default App
